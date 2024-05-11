3fda4608ae0d9a703405a3c009a938814e1e71ef
Denis <vin_ogr@list.ru>
2024-05-11 21:31:38 +0700

    Исправление Дополнительное задание по модулю 2

--- addwork.py
+++ addwork.py
@@ -1,15 +1,20 @@
 def pass_by_code(code):
-    pass_dict = {'3' : '12', '4' : '13', '5' : '1423', '6' : '121524', '7' : '162534', '8' : '13172635',
-    '9' : '1218273645', '10' : '141923283746', '11' : '11029384756', '12' : '12131511124210394857',
-    '13' : '112211310495867', '14' : '1611325212343114105968', '15' : '1214114232133124115106978',
-    '16' : '1317115262143531341251161079', '17' : '11621531441351261171089',
-    '18' : '12151811724272163631545414513612711810', '19' : '118217316415514613712811910',
-    '20' : '13141911923282183731746416515614713812911'}
-    return pass_dict.get(code)
+    pass_code = 0
+    for i in range(1, code):
+        for j in range(i+1, code):
+             if code % (i+j)  == 0:
+                pass_code = add_digit_right(pass_code,i, j)
+
+    return pass_code
 
 def rand_stone():
     import random
-    stone = str(random.randint(3, 20))
+    stone = random.randint(3, 20)
+    print(stone)
     return stone
+
+def add_digit_right(sum_number, add_number1, add_number2):
+    sum_string = str(sum_number) + str(add_number1) + str(add_number2)
+    return int(sum_string)
 
 print(pass_by_code(rand_stone()))
