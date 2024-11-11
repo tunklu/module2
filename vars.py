b15ce6cdb06b2651e44cb6592ea64fb8e2f2ca32
Denis <vin_ogr@list.ru>
2024-11-11 13:44:24 +0700

    Переменные

---
+++ vars.py
@@ -0,0 +1,5 @@
+hw_count = 12
+hours_spend = 1.5
+course_name = 'Python'
+one_course_time =  hours_spend / hw_count
+print(f'Python, всего задач:{hw_count}, затрачено часов: {hours_spend}, среднее время выполнения {one_course_time} часа.')
