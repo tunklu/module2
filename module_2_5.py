4fa81e95e5e2ce02f91b4e4ce6a953c3ff1b8724
Denis <vin_ogr@list.ru>
2024-11-12 14:35:26 +0700

    Функции в Python.Функция с параметром

---
+++ module_2_5.py
@@ -0,0 +1,13 @@
+def get_matrix(n,m,value):
+    matrix = []
+    for i in range(n):
+        matrix.extend([])
+        for j in range(m):
+            matrix.insert(j, value)
+    return matrix
+result1 = get_matrix(2, 2, 10)
+result2 = get_matrix(3, 5, 42)
+result3 = get_matrix(4, 2, 13)
+print(result1)
+print(result2)
+print(result3)
